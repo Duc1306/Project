daa8d8e1bf656e964f6466720797d65e04193be2
Duc1306 <Duc1306@gmail.com>
2025-03-08 17:37:25 +0700

    làm  danh sách sản phẩm bên client

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
-
 require("dotenv").config();
 
+const database = require("./config/database");
 const route = require('./routes/client/index.route');
+
+database.connect();
+
 const app = express();
 const port =process.env.PORT;
 
 app.set("views","./views");
-app.set("view engine", "pug");
+app.set("view engine", "pug");  
+
+app.use(express.static("public")) // nhunsg file tĩnh 
 
 route(app);
 
