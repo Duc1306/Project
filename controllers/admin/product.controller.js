184c32c53a1e79b9c7e77270bfe9de42672857d8
Duc1306 <Duc1306@gmail.com>
2025-03-10 17:44:40 +0700

    lấy data sản phẩm in ra giao diện admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,11 @@
+const Product = require("../../model/product.model");
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  const products = await Product.find({});
+  console.log(products);
   res.render("admin/pages/products/index", {
     pageTitle: " Danh sach sản phẩm",
+    products: products,
   });
 };
