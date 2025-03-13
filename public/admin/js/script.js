a27e164606a2bb498cd1b02ba25c357e1f147ddc
Duc1306 <Duc1306@gmail.com>
2025-03-13 18:38:44 +0700

    Tính năng phân trang

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -36,3 +36,20 @@ if (formSearch) {
     window.location.href = url.href;
   });
 }
+
+// End form-sreach
+
+//Pagination
+const buttonsPagination = document.querySelectorAll("[button-pagination]");
+if (buttonsPagination) {
+  let url = new URL(window.location.href);
+  buttonsPagination.forEach((button) => {
+    button.addEventListener("click", () => {
+      const page = button.getAttribute("button-pagination");
+
+      url.searchParams.set("page", page);
+      window.location.href = url.href;
+    });
+  });
+}
+//End Pagination
